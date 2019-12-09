3e241c52b364b216f4690f4353b25b32b9bb58d6
vladimir <vladimir.randjelovic@kireygroup.com>
2019-12-09 10:23:00 +0100

    Laser-AutoAssociative project created

--- src/LaserAutoAssociative/Main.java
+++ src/LaserAutoAssociative/Main.java
@@ -12,9 +12,13 @@ public class Main {
 		boolean supervised = false;
 
 //		String filePath = "G:/Artificial Intelligence/Software/RomePrograms/ArtificialIntelligence-Rome/Resources/laser-01.csv";
-		String filePath = "D:/MyDoc/Projects/ArtificialIntelligence/Resources/laser-01.csv";
-		BpAnn bp = new BpAnn(filePath, 12, 7, 0.75, new int[] { 7, 7, 7 }, 0.1, supervised);
+		String filePath = "G:/Artificial Intelligence/Software/RomePrograms/ArtificialIntelligence-Rome/Resources/laser-02-AntiEdema.csv";
+//		String filePath = "D:/MyDoc/Projects/ArtificialIntelligence/Resources/laser-01.csv";
+//		String datasetFilename, int datasetDimensionY, int datasetDimensionX, double testTraingRatio,int[] numberOfLayerNodes, double learningCoeffitient, boolean supervised
+//		BpAnn bp = new BpAnn(filePath, 12, 7, 0.75, new int[] { 7, 7, 7 }, 0.1, supervised);
+		BpAnn bp = new BpAnn(filePath, 18, 4, 1, new int[] { 4, 4, 4 }, 0.1, supervised);
 
+		
 		// System.out.println("\n\nTesting dataset");
 		// Utility.printMatrix(bp.getTestingDataset());
 		
@@ -51,25 +55,32 @@ public class Main {
 
 		if (!supervised) {
 			System.out.println("\n\n---------------------------------------Testing unsupervised - auto-associative backpropagation");
-			for (int y = 0; y < bp.getTestingDataset().length; y++) {
-				bp.classify(bp.getTestingDataset()[y]);
+			for (int y = 0; y < 100; y++) {
+				double[] a = {Math.random()*1+10, Math.random()*1+10, Math.random()*1+10, Math.random()*1+10};
+				bp.classify(a);
 				System.out.println("" + y + ": " 
-						+ bp.getTestingDataset()[y][0] + " " 
-						+ bp.getTestingDataset()[y][1] + " " 
-						+ bp.getTestingDataset()[y][2] + " " 
-						+ bp.getTestingDataset()[y][3] + " "
-						+ bp.getTestingDataset()[y][4] + " " 
-						+ bp.getTestingDataset()[y][5] + " " 
-						+ bp.getTestingDataset()[y][6] + " " 
-						+ "  ---   "	+ " Estimation: " 
 						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][0] + " "
 						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][1] + " "
 						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][2] + " "
-						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][3] + " "
-						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][4] + " "
-						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][5] + " "
-						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][6]);
+						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][3]);
 			}
 		}
+		
+//		if (!supervised) {
+//			System.out.println("\n\n---------------------------------------Testing unsupervised - auto-associative backpropagation");
+//			for (int y = 0; y < bp.getTestingDataset().length; y++) {
+//				bp.classify(bp.getTestingDataset()[y]);
+//				System.out.println("" + y + ": " 
+//						+ bp.getTestingDataset()[y][0] + " " 
+//						+ bp.getTestingDataset()[y][1] + " " 
+//						+ bp.getTestingDataset()[y][2] + " " 
+//						+ bp.getTestingDataset()[y][3] + " "
+//						+ "  ---   "	+ " Estimation: " 
+//						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][0] + " "
+//						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][1] + " "
+//						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][2] + " "
+//						+ bp.getNodes()[bp.getNumberOfLayerNodes().length - 1][3]);
+//			}
+//		}
 	}
 }
